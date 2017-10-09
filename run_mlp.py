36a543f1500b87326288c13c5790db65ef6dda6e
吴启凌 <wuqiling97@163.com>
2017-10-09 19:37:51 +0800

    让solve_net中的函数返回loss, accuracy

--- run_mlp.py
+++ run_mlp.py
@@ -8,7 +8,7 @@ from load_data import load_mnist_2d
 
 train_data, test_data, train_label, test_label = load_mnist_2d('data')
 
-# Your model defintion here
+# Your model definition here
 # You should explore different model architecture
 layer1_out = 128
 model = Network()
@@ -31,15 +31,23 @@ config = {
     'momentum': 0.9,
     'batch_size': 100,
     'max_epoch': 100,
-    'disp_freq': 50,  # display info each disp_freq training
+    'disp_freq': 600,  # display info each disp_freq training
     'test_epoch': 5
 }
 
+train_loss = []
+train_acc = []
+test_loss = []
+test_acc = []
 
-for epoch in range(config['max_epoch']):
-    LOG_INFO('Training @ %d epoch...' % (epoch))
-    train_net(model, loss, config, train_data, train_label, config['batch_size'], config['disp_freq'])
+for epoch in range(config['max_epoch']+1):
+    LOG_INFO('Training @ %d epoch...' % epoch)
+    train_res = train_net(model, loss, config, train_data, train_label, config['batch_size'], config['disp_freq'])
+    train_loss.append(train_res[0])
+    train_acc.append(train_res[1])
 
     if epoch % config['test_epoch'] == 0:
-        LOG_INFO('Testing @ %d epoch...' % (epoch))
-        test_net(model, loss, test_data, test_label, config['batch_size'])
+        LOG_INFO('Testing @ %d epoch...' % epoch)
+        test_res = test_net(model, loss, test_data, test_label, config['batch_size'])
+        test_loss.append(test_res[0])
+        test_acc.append(test_res[1])
--- solve_net.py
+++ solve_net.py
@@ -1,5 +1,6 @@
 from utils import LOG_INFO, onehot_encoding, calculate_acc
 import numpy as np
+import time
 
 
 def data_iterator(x, y, batch_size, shuffle=True):
@@ -13,10 +14,11 @@ def data_iterator(x, y, batch_size, shuffle=True):
 
 
 def train_net(model, loss, config, inputs, labels, batch_size, disp_freq):
-
     iter_counter = 0
     loss_list = []
     acc_list = []
+    epoch_loss = []
+    epoch_acc = []
 
     for input, label in data_iterator(inputs, labels, batch_size):
         target = onehot_encoding(label, 10)
@@ -37,12 +39,16 @@ def train_net(model, loss, config, inputs, labels, batch_size, disp_freq):
         acc_value = calculate_acc(output, label)
         loss_list.append(loss_value)
         acc_list.append(acc_value)
+        epoch_loss.append(loss_value)
+        epoch_acc.append(acc_value)
 
         if iter_counter % disp_freq == 0:
             msg = '  Training iter %d, batch loss %.4f, batch acc %.4f' % (iter_counter, np.mean(loss_list), np.mean(acc_list))
             loss_list = []
             acc_list = []
             LOG_INFO(msg)
+
+    return np.mean(epoch_loss), np.mean(epoch_acc)
 
 
 def test_net(model, loss, inputs, labels, batch_size):
@@ -57,5 +63,8 @@ def test_net(model, loss, inputs, labels, batch_size):
         loss_list.append(loss_value)
         acc_list.append(acc_value)
 
-    msg = '    Testing, total mean loss %.5f, total acc %.5f' % (np.mean(loss_list), np.mean(acc_list))
+    epoch_loss = np.mean(loss_list)
+    epoch_acc = np.mean(acc_list)
+    msg = '    Testing, total mean loss %.5f, total acc %.5f' % (epoch_loss, epoch_acc)
     LOG_INFO(msg)
+    return epoch_loss, epoch_acc
